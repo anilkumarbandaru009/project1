7bb8c38c4af9392001b6a58842853d7cece37f6f
Anil <anilkumarbandaru009@gmail.com>
2022-09-03 17:09:37 +0530

    code clean up

--- hello_world.c
+++ hello_world.c
@@ -4,14 +4,9 @@ void fun(){
 	//This is second comment for fun function
 	printf("this is fun function\n");
 }
-
 //comment added here 
 int main(void){
 	printf("Hello world!\n");
 	fun();
 	return 0;
-<<<<<<< HEAD
 }
-=======
-	}
->>>>>>> d61a82baef64a76b4649780a8f51d974dab474ea
